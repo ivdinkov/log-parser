cc110b40533992ea396274448659b2f4445d422c
Ivan Dinkov <Ivan.Dinkov@docusign.com>
2020-10-02 18:20:05 +0100

    added ui changes and zip handling

--- LogParser/Pages/Index.cshtml.cs
+++ LogParser/Pages/Index.cshtml.cs
@@ -1,10 +1,12 @@
 ﻿using LogParser.models;
 using LogParser.Service;
-using LogParser.Utilities;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -13,8 +15,7 @@ namespace LogParser.Pages
     public class IndexModel : PageModel
     {
         private readonly ILogRepository _logRepository;
-        private readonly long _fileSizeLimit;
-        private readonly string[] _permittedExtensions = { ".txt", ".zip" };
+        private byte[] fileContent;
 
         [BindProperty]
         public BufferedMultipleFileUploadDb FileUpload { get; set; }
@@ -29,7 +30,6 @@ namespace LogParser.Pages
         public IndexModel(IConfiguration config, ILogRepository logRepository)
         {
             _logRepository = logRepository;
-            _fileSizeLimit = config.GetValue<long>("FileSizeLimit");
         }
 
 
@@ -52,10 +52,33 @@ namespace LogParser.Pages
 
             foreach (var formFile in FileUpload.FormFiles)
             {
-                var fileContent =
-                    await FileHelpers.ProcessFormFile<BufferedMultipleFileUploadDb>(
-                        formFile, ModelState, _permittedExtensions,
-                        _fileSizeLimit);
+                var ext = Path.GetExtension(formFile.FileName).ToLowerInvariant();
+
+                try
+                {
+                    if (ext.Equals(".zip"))
+                    {
+                        using var memoryStream = formFile.OpenReadStream();
+                        using var archive = new ZipArchive(memoryStream, ZipArchiveMode.Read);
+                        foreach (ZipArchiveEntry entry in archive.Entries)
+                        {
+                            var unzippedFile = entry.Open();
+                            using var stream = new MemoryStream();
+                            await unzippedFile.CopyToAsync(stream);
+                            await SaveLogs(stream);
+                        }
+                    }
+                    else
+                    {
+                        using var memoryStream = new MemoryStream();
+                        await formFile.CopyToAsync(memoryStream);
+                        await SaveLogs(memoryStream);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    ModelState.AddModelError(string.Empty, ex.Message);
+                }
 
                 if (!ModelState.IsValid)
                 {
@@ -64,13 +87,17 @@ namespace LogParser.Pages
                                             .Select(x => x.ErrorMessage));
                     return Page();
                 }
-
-                await _logRepository.SaveAllLogsAsync(fileContent);
             }
 
             Logs = await _logRepository.GetAllLogsAsync();
 
             return Page();
+        }
+
+        private async Task SaveLogs(MemoryStream stream)
+        {
+            fileContent = stream.ToArray();
+            await _logRepository.SaveAllLogsAsync(fileContent);
         }
 
         public async Task<IActionResult> OnGetReData()
